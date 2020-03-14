7f314c7b37b6b4dcc03ae220040ffa2e5da1d28a
Petr Bouchal <pbouchal@gmail.com>
2020-03-14 18:55:49 +0100

    finalise and document drat inserter

--- R/rhub_to_drat.R
+++ R/rhub_to_drat.R
@@ -21,15 +21,32 @@ rhub_binary <- function(check_object, download = TRUE) {
   if(download) {
     td <- tempdir()
     tf <- paste0(td, "/", package_name, "_", package_version, ext)
-    download.file(binurl, destfile = tf)
+    utils::download.file(binurl, destfile = tf)
     return(tf)
   } else {
     return(binurl)
   }
 }
 
+#' Put source and binary package into drat
+#'
+#'Build source and Mac binary bundle locally and Windows binary on rhub and put them into a drat repository.
+#'
+#' @param pkg path to package directory
+#' @param last_tag whether to check out the latest tag (which should point to a release)
+#' @param repodir directory containing drat repo. Looks for the `dratRepo` option, if not found, defaults to "~/github/drat"
+#' @param build_win (TRUE) whether to build the Windows binary on rhub and put it into drat.
+#'
+#' @return TRUE on success
+#' @examples
+#' \dontrun{
+#' insert_package_to_drat("~/github/czso", last_tag = T, build_win = T)
+#' }
+#' # ADD_EXAMPLES_HERE
 #' @export
-insert_binary_to_drat <- function(pkg, last_tag = T) {
+insert_package_into_drat <- function(pkg, last_tag = T,
+                                   repodir = getOption("dratRepo", default = "~/github/drat"),
+                                   build_win = T) {
   origwd <- getwd()
   setwd(pkg)
   if(last_tag) {
@@ -40,18 +57,27 @@ insert_binary_to_drat <- function(pkg, last_tag = T) {
     print(git2r::status())
   }
 
-  maccheck <- rhub::check_on_macos(pkg)
-  wincheck <- rhub::check_on_windows(pkg)
-  ubuntucheck <- rhub::check_on_ubuntu(pkg)
+  tryCatch({bin_build <- devtools::build(pkg, binary = T, args = c('--preclean'))
+            drat::insertPackage(bin_build, repodir = repodir)}, error=function(e){})
 
-  drat::insertPackage(rhub_binary(maccheck, download = T))
-  drat::insertPackage(rhub_binary(wincheck, download = T))
-  drat::insertPackage(rhub_binary(ubuntucheck, download = T))
+  src_build <- devtools::build(pkg)
+  drat::insertPackage(src_build, repodir = repodir)
 
   if(last_tag) {
     git2r::checkout(pkg, branch = "master")
     print(git2r::status())
   }
+
+  # maccheck <- rhub::check_on_macos(src_build)
+
+  # ubuntucheck <- rhub::check_on_ubuntu(src_build)
+  # drat::insertPackage(rhub_binary(ubuntucheck, download = T), repodir = repodir)
+
+  if(build_win) wincheck <- rhub::check_on_windows(src_build)
+
+  if(build_win) drat::insertPackage(rhub_binary(wincheck, download = T), repodir = repodir)
+
   setwd(origwd)
+  return(TRUE)
 }
 
