e0d8c4240e25b6f4635c9ae3bd4613cbba901ec1
Petr Bouchal <pbouchal@gmail.com>
2020-06-06 13:12:37 +0200

    file helpers

---
+++ R/utils.R
@@ -0,0 +1,20 @@
+# Helper functions from bookdown and rticles -----------------------------------
+# copied from https://github.com/atlas-aai/ratlas/
+
+find_file <- function(template, file) {
+  template <- system.file("rmarkdown", "templates", template, file,
+                          package = "ptrr")
+  if (template == "") {
+    stop("Couldn't find template file ", template, "/", file, call. = FALSE)
+  }
+
+  template
+}
+
+find_resource <- function(template, file) {
+  find_file(template, file.path("resources", file))
+}
+
+ptrr_file <- function(...) {
+  system.file(..., package = "ptrr", mustWork = TRUE)
+}
