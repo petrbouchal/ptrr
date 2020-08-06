3a86aaf608d6e3177dedf9214c7834f0819eb001
Petr Bouchal <pbouchal@gmail.com>
2020-08-06 18:02:31 +0200

    implement rich text + axis hiding

--- R/theme.R
+++ R/theme.R
@@ -85,23 +85,30 @@
 #' @export
 theme_ptrr <- function(gridlines = c("y", "x", "both", "scatter"),
                          base_size = 11,
-                         family = "Roboto Condensed", title_family = "Roboto",
+                         family = "Roboto Condensed",
+                         title_family = "Roboto",
                          margin_side = 6,
                          margin_bottom = 6,
                          plot.title.position = "plot",
-                         axis.title = ggplot2::element_blank(),
+                         axis_titles = FALSE,
                          multiplot = FALSE,
+                         richtext = FALSE,
                          ...) {
   tonecol <- "#e3f1ff"
+
+  element_switch <- if(richtext) ggtext::element_markdown else ggplot2::element_text
+
   grd <- match.arg(gridlines)
   grid_col <- if(grd == "scatter" | multiplot) "white" else "grey92"
   bg_col <- if(grd == "scatter" | multiplot) tonecol else "white"
   element_gridline <- ggplot2::element_line(colour = grid_col, size = 0.3)
   thm <- ggplot2::theme_minimal(base_size = base_size, base_family = family) +
     ggplot2::theme(plot.title.position = plot.title.position,
-                   plot.title = ggplot2::element_text(face = "bold",
+                   plot.title = element_switch(face = "bold",
                                                       size = base_size * 1.2,
                                                       family = title_family),
+                   plot.subtitle = element_switch(),
+                   plot.caption = element_switch(colour = "grey60"),
                    panel.grid.minor = ggplot2::element_blank(),
                    panel.grid.major.x = if(grd != "y")
                      element_gridline else ggplot2::element_blank(),
@@ -110,8 +117,12 @@ theme_ptrr <- function(gridlines = c("y", "x", "both", "scatter"),
                    # axis.line = ggplot2::element_line(),
                    panel.background = ggplot2::element_rect(fill = bg_col,
                                                             colour = NA),
-                   axis.title = axis.title,
-                   strip.text.x = ggplot2::element_text(hjust = 0),
+                   axis.title = if(axis_titles) element_switch() else ggplot2::element_blank(),
+                   # text = element_switch(),
+                   strip.text.x = element_switch(hjust = 0),
+                   strip.text.y = element_switch(hjust = 0),
+                   axis.text.x = element_switch(),
+                   axis.text.y = element_switch(),
                    plot.margin = ggplot2::unit(c(10, margin_side,
                                                  margin_bottom, margin_side),
                                                units = "pt"))
