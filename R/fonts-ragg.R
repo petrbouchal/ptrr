8ebdc5a8ae97a68162721ade5e465ea15726edf9
Petr Bouchal <pbouchal@gmail.com>
2021-02-21 21:53:46 +0100

    initial systemfonts functions

---
+++ R/fonts-ragg.R
@@ -0,0 +1,34 @@
+#' @export
+register_plexsans_condensed <- function(family, features) {
+  systemfonts::register_font(family,
+                             plain = system.file("fonts", "plex",
+                                                 "IBMPlexSansCondensed-Regular.ttf",
+                                                 package = "ptrr"),
+                             italic = system.file("fonts", "plex",
+                                                  "IBMPlexSansCondensed-Italic.ttf",
+                                                  package = "ptrr"),
+                             bold = system.file("fonts", "plex",
+                                                "IBMPlexSansCondensed-Bold.ttf",
+                                                package = "ptrr"),
+                             bolditalic = system.file("fonts", "plex",
+                                                      "IBMPlexSansCondensed-BoldItalic.ttf",
+                                                      package = "ptrr")
+  )
+}
+
+#' @export
+register_plexsans <- function(family, features) {
+  systemfonts::register_font("IBM Plex Sans Condensed",
+                             plain = system.file("fonts", "plex",
+                                                 "IBMPlexSans-Regular.ttf",
+                                                 package = "ptrr"),
+                             italic = system.file("fonts", "plex",
+                                                  "IBMPlexSans-Italic.ttf",
+                                                  package = "ptrr"),
+                             bold = system.file("fonts", "plex",
+                                                "IBMPlexSans-Bold.ttf",
+                                                package = "ptrr"),
+                             bolditalic = system.file("fonts", "plex",
+                                                      "IBMPlexSans-BoldItalic.ttf",
+                                                      package = "ptrr")  )
+  }
