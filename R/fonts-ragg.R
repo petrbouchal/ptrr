d34223714d3bc74acf1e4e39430bf1049a8e17f9
Petr Bouchal <pbouchal@gmail.com>
2021-02-21 23:14:06 +0100

    safeguard against font name clash

--- R/fonts-ragg.R
+++ R/fonts-ragg.R
@@ -1,34 +1,46 @@
 #' @export
 register_plexsans_condensed <- function(family = "IBM Plex Sans", features = NULL) {
-  systemfonts::register_font(family,
-                             plain = system.file("fonts", "plex",
-                                                 "IBMPlexSansCondensed-Regular.ttf",
-                                                 package = "ptrr"),
-                             italic = system.file("fonts", "plex",
-                                                  "IBMPlexSansCondensed-Italic.ttf",
+
+  if(family %in% systemfonts::system_fonts()[["family"]]) {
+    usethis::ui_warn(" Family {usethis::ui_value(family)} already exists in system font registry. Skipping.")
+  } else {
+
+    systemfonts::register_font(family,
+                               plain = system.file("fonts", "plex",
+                                                   "IBMPlexSansCondensed-Regular.ttf",
+                                                   package = "ptrr"),
+                               italic = system.file("fonts", "plex",
+                                                    "IBMPlexSansCondensed-Italic.ttf",
+                                                    package = "ptrr"),
+                               bold = system.file("fonts", "plex",
+                                                  "IBMPlexSansCondensed-Bold.ttf",
                                                   package = "ptrr"),
-                             bold = system.file("fonts", "plex",
-                                                "IBMPlexSansCondensed-Bold.ttf",
-                                                package = "ptrr"),
-                             bolditalic = system.file("fonts", "plex",
-                                                      "IBMPlexSansCondensed-BoldItalic.ttf",
-                                                      package = "ptrr")
-  )
+                               bolditalic = system.file("fonts", "plex",
+                                                        "IBMPlexSansCondensed-BoldItalic.ttf",
+                                                        package = "ptrr")
+    )
+  }
+
 }
 
 #' @export
 register_plexsans <- function(family = "IBM Plex Sans", features = NULL) {
-  systemfonts::register_font(family,
-                             plain = system.file("fonts", "plex",
-                                                 "IBMPlexSans-Regular.ttf",
-                                                 package = "ptrr"),
-                             italic = system.file("fonts", "plex",
-                                                  "IBMPlexSans-Italic.ttf",
+
+  if(family %in% systemfonts::system_fonts()[["family"]]) {
+    usethis::ui_warn(" Family {usethis::ui_value(family)} already exists in system font registry. Skipping.")
+  } else {
+
+    systemfonts::register_font(family,
+                               plain = system.file("fonts", "plex",
+                                                   "IBMPlexSans-Regular.ttf",
+                                                   package = "ptrr"),
+                               italic = system.file("fonts", "plex",
+                                                    "IBMPlexSans-Italic.ttf",
+                                                    package = "ptrr"),
+                               bold = system.file("fonts", "plex",
+                                                  "IBMPlexSans-Bold.ttf",
                                                   package = "ptrr"),
-                             bold = system.file("fonts", "plex",
-                                                "IBMPlexSans-Bold.ttf",
-                                                package = "ptrr"),
-                             bolditalic = system.file("fonts", "plex",
-                                                      "IBMPlexSans-BoldItalic.ttf",
-                                                      package = "ptrr")  )
-  }
+                               bolditalic = system.file("fonts", "plex",
+                                                        "IBMPlexSans-BoldItalic.ttf",
+                                                        package = "ptrr")  )}
+}
